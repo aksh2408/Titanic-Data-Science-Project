d727a9893ec6270320ad9173b7403f8de3561620
Akshay Kumar Sridhar <vinceakshay@gmail.com>
2020-08-31 22:45:19 +0200

    Updated Joke

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,11 +2,21 @@ import pandas as pd
 import numpy as np
 
 def execute(input_file, output_file):
-    data = pd.read_csv(input_file, sep = ";")
-    data = data.dropna()
-    del(data["Name"])
-    del(data["Ticket"])
-    del(data["Cabin"])
+    '''
+    Data preprocessing stage: Deleting the unwanted anamolies
+    '''
     
+    data = pd.read_csv('C:/Users/Akshay/train.csv', sep = ";")
+    
+    '''
+    Deleting or dropping out the null values
+    '''
+    data = data.dropna()
+    '''
+    Deleting the unwanted columns as it is mainly consists of strings
+    '''
+    data = data.drop(["Name","Ticket", "Cabin"], axis = 1, inplace = True)
 
     data.to_csv(output_file)
+
+execute('train.csv', 'testrain.csv')
